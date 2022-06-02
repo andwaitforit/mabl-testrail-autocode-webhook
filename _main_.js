47de68a9dcc58dc1cf348cc0d3b1124a99168901
andwaitforit <35976286+andwaitforit@users.noreply.github.com>
2022-06-02 10:10:17 -0400

    Update _main_.js
    Improved variable naming and comments

--- _main_.js
+++ _main_.js
@@ -1,4 +1,3 @@
-
 const lib = require('lib',)({token: process.env.STDLIB_SECRET_TOKEN});
 const axios = require('axios');
 /**
@@ -9,52 +8,60 @@ module.exports = async (context) => {
   let result = {}
 
   console.log('params:', context.params);
+
+  const user_name = process.env.user_name;
+  const password = process.env.password;
+  let base_url = "https://myexample.testrail.io/index.php?/api/v2/";
   
   //Find number of tests to iterate through
-  let numTests = (context.params.journeys.length);
-  console.log(numTests);
+  let test_cases_executed = (context.params.journeys.length);
+  console.log(test_cases_executed);
   
   
-  for (let i = 0; i < numTests; i++) {
+  for (let i = 0; i < test_cases_executed; i++) {
     
     console.log('Test'+i, context.params.journeys[i].id);
     console.log('Test'+i + 'Status',context.params.results[i].success);
     console.log('Testcase id' + i, context.params.journey_executions[i].test_cases[0].id)
     
     //Build api endpoint and request data
-    var testResult = context.params.results[i].success;
-    var status_id;
-    if (testResult== false){
-      status_id = 5;
+    let mabl_test_result_status = context.params.results[i].success;
+    let testrail_test_case_result_status;
+    if (mabl_test_result_status== false){
+      testrail_test_case_result_status = 5;
     }
-    else if(testResult == true){
-      status_id = 1;
+    else if(mabl_test_result_status == true){
+      testrail_test_case_result_status = 1;
     }
     else{
-      status_id = 2;
+      testrail_test_case_result_status = 2;
     }
     
     
-    var tcId = context.params.journey_executions[i].test_cases[0].id;
-    var url = "https://myInstance.testrail.com/index.php?/api/v2/add_result/" + tcId;
-    var uname = process.env.uname;
-    var pass = process.env.pass;
+    let testrail_test_case_id = context.params.journey_executions[i].test_cases[0].id;
+    testrail_test_case_id = testrail_test_case_id.replace( /^\D+/g, '');
+
     
-    let postData = JSON.stringify({
-      "status_id": status_id,
+    let testrail_add_result_url = base_url + "add_result/" + testrail_test_case_id;
+    
+    //Generate the JSON body to pass to testrail api call to add result of mabl executed test case
+    let testrail_add_result_body = JSON.stringify({
+      "testrail_test_case_result_status": testrail_test_case_result_status,
       "comment": "This is an automatic mabl result from webhooks"
     })
-    console.log(postData);
-    let resp = await axios.post(url, postData, {
+    console.log(testrail_add_result_body);
+
+    //Post request to testrail to add result to project test run with mabl execution results   
+    let testrail_add_result_response = await axios.post(testrail_add_result_url, testrail_add_result_body, {
         auth: {
-          username: uname,
-          password: pass
+          username: user_name,
+          password: password
         },
         headers: {
             'Content-Type': 'application/json' 
           }
       });
-      console.log(resp);    
+      console.log(testrail_add_result_response);    
   }
   console.log('headers:', context.http.headers);
   console.log('body:', context.http.body);
@@ -62,4 +69,3 @@ module.exports = async (context) => {
     
   return result;
 };
-
